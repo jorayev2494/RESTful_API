96a430f97edd10b6b797e8881b6ec149bd5c2f1b
jorayev2494@gmail.com <jorayev2494@gmail.com>
2018-12-25 18:21:24 +0200

    Auth RESTful API

---
+++ app/User.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password', "api_token"
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * Генерация токена для Пользователя
+     *
+     * @return void
+     */
+    public function generateToken()
+    {
+        $this->api_token = str_random(60);
+        // dd($this->api_token);
+        $this->save();
+
+        return $this->api_token;
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Http\Request;
+use App\Article;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post("register", 'Auth\RegisterController@register');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout');
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Auth::guard('api')->user(); // instance of the logged user
+Auth::guard('api')->check(); // if a user is authenticated
+Auth::guard('api')->id(); // the id of the authenticated user
+
+// Auth::routes();
+// Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => 'auth:api'], function() {
+    Route::get('articles', 'ArticleController@index');
+    Route::get('articles/{article}', 'ArticleController@show');
+    Route::post('articles', 'ArticleController@store');
+    Route::put('articles/{article}', 'ArticleController@update');
+    Route::delete('articles/{article}', 'ArticleController@delete');
+});
+
+
+
